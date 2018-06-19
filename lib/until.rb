c5a129d25d05d0988ca642480cacd4042c53fa71
abreternitz <aliceburian@gmail.com>
2018-06-19 21:00:25 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,12 +1,10 @@
 def using_until
   levitation_force = 6
-  loop do
     until levitation_force==10
       puts "Wingardium Leviosa"
       levitation_force=levitation_force + 1
     end
     break
-  end
 end
 
 using_until
